0f431a00c3851c35ee246910c5e2809b95d8e143
AceHunterr <aggarwalmehul26@gmail.com>
2023-05-26 03:21:12 +0530

    Added Scan Resume Page

--- src/components/sidenav.jsx
+++ src/components/sidenav.jsx
@@ -16,9 +16,9 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
-
 import MailIcon from '@mui/icons-material/Mail';
 import HomeIcon from '@mui/icons-material/Home';
 import WorkIcon from '@mui/icons-material/Work';
--- src/pages/Scan_Resume.jsx
+++ src/pages/Scan_Resume.jsx
@@ -1,6 +1,8 @@
 import React,{} from 'react';
 import Box from '@mui/material/Box';
 import Sidenav from '../components/sidenav';
+import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+
 
 const ScanResume = () => {
   return (
@@ -9,8 +11,15 @@ const ScanResume = () => {
     <Box component="main" sx={{ flexGrow: 1, p: 3}}>
       {/* Add your home page content here */}
 
-      <h2>ScanResume</h2>
-    
+    <div className="job-detail-header-div">
+      <h2 className='job-detail-heading'>Scan Your Resume</h2>
+    </div>
+
+    <button class="work_type_job_button apply-now " id="resume-button">
+      <span class="icon"><DocumentScannerIcon /></span>
+      <span class="text">Scan Your Resume</span>
+    </button>
+
 
     </Box>
     </Box>
